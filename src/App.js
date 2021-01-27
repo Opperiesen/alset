864f01483e2cfef1ced57534ff0b05948fe9cb53
Opperiesen <77763298+Opperiesen@users.noreply.github.com>
2021-01-27 23:26:41 +0100

    Header + assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Test</h1>
+      <Header />
+      {/* items => snapping */}
+      {/* items => snapping */}
+      {/* items => snapping */}
+      {/* items => snapping */}
     </div>
   );
 }
